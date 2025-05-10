14a1d0f475dcf6c20879b5ff84315a7bc7bea430
DanielLenoz <danielstivenxx@outlook.com>
2025-05-10 14:38:54 -0500

    [UPDATE] add the proyects to the settings.py and the docments drf

--- trello/trello/settings.py
+++ trello/trello/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_spectacular',
+    'boards',
+    'listsTrello',
+    'cards',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
