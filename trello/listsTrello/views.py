493e36246cd081c224e6c0d6a4e4ce6c396c828a
DanielLenoz <danielstivenxx@outlook.com>
2025-05-10 14:30:22 -0500

    [CREATE] the viewsets

--- trello/listsTrello/views.py
+++ trello/listsTrello/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ListsTrelloSerializer
+from .models import ListsTrello
 
-# Create your views here.
+
+class ListsTrelloViewSet(viewsets.ModelViewSet):
+    queryset = ListsTrello.objects.all()
+    serializer_class = ListsTrelloSerializer
